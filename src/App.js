8d2c88ff1f3e4342eee2b7cdbce1bbb10cfe52e4
renatogsouza <renatomacedo1995@gmail.com>
2021-04-28 16:27:38 -0300

    acho que acabo jaja

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
+  const movies = [
+    {
+      title: 'Steve Jobs',
+      subtitle: ' Steve Jobs (2015)',
+      storyline: 'Movie Storyline 1',
+      rating: 4.5,
+      imagePath: 'images/movie_1',
+    },
+    {
+      title: 'Movie Title 2',
+      subtitle: 'Movie Subtitle 2',
+      storyline: 'Movie Storyline 2',
+      rating: 4.5,
+      imagePath: 'images/movie_2',
+    },
+    {
+      title: 'Movie Title 3',
+      subtitle: 'Movie Subtitle 3',
+      storyline: 'Movie Storyline 3',
+      rating: 3,
+      imagePath: 'images/movie_3',
+    },
+  ];
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <Header />
+      <MovieList movies={ movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+
+    const renderMovieCard = movies.map((movie) => (<MovieCard
+      movie={ movie }
+      key={ movie.title }
+    />));
+    return (
+      renderMovieCard
+    );
+  }
+}
+
+export default MovieList;
