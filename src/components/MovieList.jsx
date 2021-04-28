2b8bf0c5963ada26c98523b47ab0c9f01f6cc3d9
André De Bem <andrebemferraz@gmail.com>
2021-04-28 16:19:12 -0300

    Request III

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const arrayMovies = this.props.movies;
+    return (
+      <h1>TESTE!</h1>
+    )
+  }
+}
+
+MovieList.propTypes = {
+  arrayMovies: PropTypes.array.isRequired,
+}
+
+export default MovieList;
