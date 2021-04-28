1961459cb6a542696d317fcbae53ff495b3c0ec0
João Victor Pistorio <joao.pistorio@gmail.com>
2021-04-28 17:04:04 -0300

    Commit bomb sorry

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
-// implement MovieCard component here
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+
+    return (
+      <section>
+        <img src={ imagePath } alt="movie cartaz" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <section>
+        { movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />)) }
+      </section>
+    );
+  }
+}
+
+export default MovieList;
