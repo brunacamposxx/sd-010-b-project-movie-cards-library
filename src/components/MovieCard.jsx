ae3fc7eca6cbc7785214ab36e1068b19844fb0d2
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-04-28 20:23:54 -0300

    adicionando desafios & desestruturando conteudo ps, preciso adicionar os agradecimentos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-// import Rating from './Rating'
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    // const { movies: { title, subtitle, storyline, rating, imagePath }, } = this.props;
-    const { movie } = this.props;
-    // const { title, subtitle, storyline, rating, imagePath } = movies;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
       <li>
-        <img src={ movie.imagePath } alt={ movie.title } />
-        <h4>{ movie.title }</h4>
-        <h5>{ movie.subtitle }</h5>
-        <p>{ movie.storyline }</p>
-        {/* <Rating movie={ movie }/> */}
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </li>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 class Rating extends React.Component {
-    render() {
-        const { movie } = this.props;
-        return ()
-    }
+  render() {
+    const { rating } = this.props;
+    return (
+      <div>
+        <p className="rating">{ rating }</p>
+      </div>
+    );
+  }
 }
 
 export default Rating;
