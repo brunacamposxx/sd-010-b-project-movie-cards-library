d3465d659422b4884336b27743153fc168476de4
tomsantos07 <xotmsantosx@live.com>
2021-04-28 18:52:19 -0300

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,13 @@
-// Iniciando o projeto 🚀" > src/components/Header.jsx
-// .
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
