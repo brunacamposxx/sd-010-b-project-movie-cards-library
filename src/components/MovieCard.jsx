adc5b2593ee26db4a7c4585922a0c39dd19f17b0
brunobastosfer <brunobastos@edu.unifor.br>
2021-04-28 16:42:48 -0300

    questions

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,26 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movieInfo } = this.props;
+    return (
+      <section>
+        <p>
+          {movieInfo.title}
+        </p>
+        <p>
+          {movieInfo.subtitle}
+        </p>
+        <p>
+          {movieInfo.storyline}
+        </p>
+        <img src={ movieInfo.imagePath } alt={ movieInfo.title } />
+        <p>
+          {movieInfo.rating}
+        </p>
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
