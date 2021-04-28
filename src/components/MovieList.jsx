5cab7532e47e50e8b6126d0036eba5da65b4fd9a
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-04-28 16:02:49 -0300

    Renderiza componentes MovieCard dentro de MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,13 +2,14 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <div>
-        {movies}
+        {movies.map((movie, index) => <MovieCard key={ index } mov={ movie } />)}
       </div>
     );
   }
