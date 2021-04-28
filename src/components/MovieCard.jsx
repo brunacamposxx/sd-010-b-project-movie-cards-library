19aa78b1076a646c4a5e464ac3f40cdfc9a4c984
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-04-28 18:10:05 -0300

    proptypes movielist

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,9 @@
 // implement MovieCard component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
-const movies = [
+/* const movies = [
   {
     id: 1,
     name: 'Interestelar',
@@ -10,7 +11,7 @@ const movies = [
     description: `As reservas naturais da Terra estão chegando ao fim
     e um grupo de astronautas recebe a missão
     de verificar possíveis planetas pra receberem a população mundial.`,
-    img: 'https://wallup.net/wp-content/uploads/2019/09/367002-interstellar-adventure-mystery-sci-fi-futuristic-film-spaceship-poster-planet.jpg',
+    image: 'https://wallup.net/wp-content/uploads/2019/09/367002-interstellar-adventure-mystery-sci-fi-futuristic-film-spaceship-poster-planet.jpg',
   },
   {
     id: 2,
@@ -19,7 +20,7 @@ const movies = [
     description: `Miguel sonha em se tornar um músico como seu ídolo Ernesto de la Cruz,
     embarca em uma jornada extraordinária para desvendar
     a verdadeira história por trás da história de sua família.`,
-    img: 'https://storage.googleapis.com/adm-portal.appspot.com/noticias/_imgHighlight/832973/artigo_109249.jpg?mtime=20181125185128&focal=none',
+    image: 'https://storage.googleapis.com/adm-portal.appspot.com/noticias/_imgHighlight/832973/artigo_109249.jpg?mtime=20181125185128&focal=none',
   },
   {
     id: 3,
@@ -28,7 +29,7 @@ const movies = [
     description: `Kevin possui 23 personalidades distintas 
     Um dia, ele sequestra três adolescentes,
     em cativeiro, elas passam a conhecer as diferentes facetas de Kevin.`,
-    img: 'https://www.ezimonteiro.com.br/wp-content/uploads/2017/04/split.jpg',
+    image: 'https://www.ezimonteiro.com.br/wp-content/uploads/2017/04/split.jpg',
   },
   {
     id: 4,
@@ -37,7 +38,7 @@ const movies = [
     description: `Lucy embarca numa viagem para conhecer os pais do seu namorado Jake.
     Chegando no local, ela começa a notar comportamentos estranhos
     e muita coisa non-sense acontece`,
-    img: 'https://www.planocritico.com/wp-content/uploads/2020/09/estou-pensando-em-acabar-com-tudo-plano-critico.jpg',
+    image: 'https://www.planocritico.com/wp-content/uploads/2020/09/estou-pensando-em-acabar-com-tudo-plano-critico.jpg',
   },
   {
     id: 5,
@@ -47,7 +48,7 @@ const movies = [
      mística de presente de seu avô. Existem apenas seis outras no mundo todo,
      e reza a lenda que será concedido um desejo a quem possuir todas as sete.
      Quando uma força obscura provoca uma tragédia.`,
-    img: 'https://aguycalledpeg.files.wordpress.com/2018/09/poster-e1537956413457.jpg?w=1000&h=582&crop=1',
+    image: 'https://aguycalledpeg.files.wordpress.com/2018/09/poster-e1537956413457.jpg?w=1000&h=582&crop=1',
   },
   {
     id: 6,
@@ -55,24 +56,8 @@ const movies = [
     subtitle: 'The Godfather',
     description: `Tem tiro, máfia e chefão.
     Nunca vi, minha mãe quem falou`,
-    img: 'https://www.metalpiracicaba.com.br/wp-content/uploads/2018/04/marlon-1-696x407.jpg',
+    image: 'https://www.metalpiracicaba.com.br/wp-content/uploads/2018/04/marlon-1-696x407.jpg',
   },
-];
-
-class MovieCard extends Component {
-  render() {
-    return (movies.map((movie) => (
-      <div className="movie-card">
-        <img className="movie-card-image" src={ movie.img } alt={ movie.name } />
-        <div className="movie-card-body">
-          <h4 className="movie-card-title">{ movie.name }</h4>
-          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
-          <p className="movie-card-storyline">{ movie.description }</p>
-        </div>
-        <Rating className ="movie-card-rating" />
-      </div>))
-    );
-  }
-}
+]; */
 
 export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,14 @@
 // implement Rating component here
 import React from 'react';
 
+const ratings = ['4.8', '5', '4.5', '2', '1.3', '4'];
+
 class Rating extends React.Component {
   render() {
-    return (
-      <div> teste</div>
+    return (ratings.map((rating) => (
+      <div className="" key={ rating.index }>
+        { rating }
+      </div>))
     );
   }
 }
