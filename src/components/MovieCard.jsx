31b224dd701eac573adfb4242330ee59353e4b45
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-04-28 18:31:13 -0300

    requisito 7, 8, 9 e 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,25 @@
 // implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <section>
+        <img src={ movie.imagePath } alt="Movie poster" />
+        <h4>
+          { movie.title }
+        </h4>
+        <h5>
+          { movie.subtitle }
+        </h5>
+        <p>
+          { movie.storyline }
+        </p>
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
