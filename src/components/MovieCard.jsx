c4ea8b920e87bebab84ee0430cad3ac730097858
Junior030 <juniorhenrique030@gmail.com>
2021-04-28 18:29:53 -0300

    Requisito 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
 // implement MovieCard component here
+
+import React from 'react';
+import MovieList from './MovieList';
+// import Data from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <MovieList>
+        <MovieCard />
+      </MovieList>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return <MovieList movies={ movies } />;
+  }
+}
+
+export default MovieList;
