9126196dc45c810ed1cf399d1b74af77067af583
Jessie <miss_sc_jessi@hotmail.com>
2021-04-28 14:46:37 -0300

    commit inicial

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
 // implement Header component here
+import React, { Component } from 'react'
+
+export class Header extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default Header
