31bab16d8b0a999df63ea6cdfc78b26d692f21eb
limaluizpaulo <limaluizpaulo@live.com>
2021-04-28 17:05:55 -0300

    Proptypes e classes ajustadas

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import '../App.css';
 
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <h1>
+      <header className="movie-card-header">
+        <h1 className="page-title">
           Movie Cards Library
         </h1>
       </header>
+
     );
   }
 }
