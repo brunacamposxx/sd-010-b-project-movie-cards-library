01fb72904ab1a81a3266fea6a2b391bb7475613f
Maximiliano Alvarenga <maximilianoabispo@gmail.com>
2021-04-28 16:59:26 -0400

    Implementado os requisitos 11, 12, 13 e 14 e ajustado o CSS.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <>
+      <section className="movie-card">
         <img src={ movie.imagePath } alt={ movie.subtitle } />
-        <h4>{ movie.title }</h4>
-        <h5>{ movie.subtitle }</h5>
-        <p>{ movie.storyline }</p>
-      </>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={ movie.rating } />
+        </div>
+      </section>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,11 +6,11 @@ class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <>
+      <section className="movie-list">
         {movies.map((movie) => (
           <MovieCard key={ movie.title } movie={ movie } />
         ))}
-      </>
+      </section>
     );
   }
 }
