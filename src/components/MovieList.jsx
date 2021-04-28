7809353b4245756de2bfb70ca0a5cab26a1d12fc
Victor Lopes <victor.cabrera0209@gmail.com>
2021-04-28 18:45:58 -0300

    First try to resolve requirement 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    const movies = this.props;
+    return <p>{movies[0]}</p>;
+  }
+}
+
+export default MovieList;
