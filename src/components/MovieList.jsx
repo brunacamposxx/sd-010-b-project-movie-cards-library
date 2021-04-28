ce64859ef990546941d52fe40dc704f852ef6a6b
DennisMarcelo <dennismarcelo15@gmail.com>
2021-04-28 18:35:11 -0300

    requisitos 4 e 5 completos

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
+
     return (
-      <div> </div>
+      <main>
+        { movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />
+        )) }
+      </main>
     );
   }
 }
+// Descobri que PropTypes.array não funciona
+// então achei essa discução no github arreseito do arrayOf:  https://github.com/facebook/prop-types/issues/212
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(Object).isRequired,
+};
 
 export default MovieList;
