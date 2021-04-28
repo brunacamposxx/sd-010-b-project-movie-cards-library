a1308d9de9b09e31a09fff39c4a8fafaa971ad93
Roger Franco <rogerfrancogalt@gmail.com>
2021-04-28 15:36:56 -0400

    questões 3 e 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-  
+    return (
+
+    );
   }
 }
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,12 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard'
 
 class MovieList extends React.Component {
   render() {
-  
+    return (
+
+    );
   }
 }
 
