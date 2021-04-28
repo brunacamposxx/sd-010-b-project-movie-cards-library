94336b249ac969342e54cf7143ef1eef08d6f382
André De Bem <andrebemferraz@gmail.com>
2021-04-28 18:00:25 -0300

    Request IV - X

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,30 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    const { key } = this.props;
+    const { movie } = this.props;
+    return (
+      <div>
+        <img src={ movie.imagePath } alt="" />
+        <h4>{ key }</h4>
+        <h5>{ movie.subtitle }</h5>
+        <p>{ movie.storyline }</p>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  key: PropTypes.string.isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import MovieCard from './MovieCard';
+
 class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
-      <h1>{ movies }</h1>
+      movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
     );
   }
 }
