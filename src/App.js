859f43a9a8e628fc97e7fd214f1e427104e8ee8c
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-04-28 18:14:20 -0300

    vqv

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+
 import './App.css';
-import Header from './components/Header.jsx'
-import MovieList from './components/MovieList.jsx'
+
+import Header from './components/Header';
+
+import MovieList from './components/MovieList';
 
 function App() {
   return (
-    <div className="">
+    <div className="App">
+      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       <Header />
       <MovieList />
     </div>
