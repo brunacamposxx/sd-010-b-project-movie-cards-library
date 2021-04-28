7c56e5d9c6a9214e5503940c659a6c2507fefd68
Roger Franco <rogerfrancogalt@gmail.com>
2021-04-28 19:25:34 -0400

    Varios exercícios concluidos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -17,13 +17,13 @@ class MovieCard extends React.Component {
     );
   }
 }
-MovieCard.propTypes = {
-  movie: propTypes.arrayOf(propTypes.shape({
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-  imagePath: PropTypes.string.isRequired,
-}))};
+MovieCard.PropTypes = {
+  movie: PropTypes.arrayOf(PropTypes.shape({
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  rating: PropTypes.number,
+  imagePath: PropTypes.string,
+})).isRequired};
 
 export default MovieCard;
