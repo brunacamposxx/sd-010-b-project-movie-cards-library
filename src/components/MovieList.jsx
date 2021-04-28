6c228fa886d4ad9201e516e66deacafe8269687c
Aremtior <marcosleandrofl@gmail.com>
2021-04-28 16:06:09 -0300

    Requisito 4 - Components <MovieCard /> rendered within MovieList.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,18 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-
+    return (
+      this.props.movies.map((movie) => <MovieCard />)
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default MovieList;
