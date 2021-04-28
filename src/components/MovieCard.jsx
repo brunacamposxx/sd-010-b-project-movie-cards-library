cf755cb2bfb2a6a050dcdfc8f92ae4e6ac1cf6e1
Antenor <antenorzapata@gmail.com>
2021-04-28 16:05:23 -0300

    requisito 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,11 +4,15 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
-    // const { movie } = this.props;
-    // const { title, subtitle, storyline, imagePath, rating } = movie;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
     return (
       <div>
         <h1>MOvieCard</h1>
+        <img src={ imagePath } alt="movie" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{ storyline }</p>
       </div>
     );
   }
@@ -17,8 +21,9 @@ class MovieCard extends Component {
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    substitle: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
     rating: PropTypes.number,
   }).isRequired,
 };
