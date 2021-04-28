92a72d4f976abf315797933940e2c8460c49d739
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-04-28 18:24:04 -0300

    test para a 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,14 +3,13 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     return (
-      <div> vaqv </div>
-    //   <div>
-    //     <p>{ this.props.title </p>
-    //     <p>{ this.props.subtitle }</p>
-    //     <p>{ this.props.storyline }</p>
-    //     <p>{ this.props.rating }</p>
-    //     <img src={ this.props.img }/>
-    //   </div>
+      <div>
+        <p>{ this.props.title </p>
+        <p>{ this.props.subtitle }</p>
+        <p>{ this.props.storyline }</p>
+        <p>{ this.props.rating }</p>
+        <img src={ this.props.img }/>
+       </div>
     );
   }
 }
