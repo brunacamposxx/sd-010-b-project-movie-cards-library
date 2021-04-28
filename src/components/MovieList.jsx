c7a950fd42e868001bedf4652c09b975ac83e3b3
Flavio Andrade <foa12381@gmail.com>
2021-04-28 16:42:56 -0300

    adiciona heder

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <ol>
+        <li>MoviaList</li>
+      </ol>
+    );
+  }
+}
+export default MovieList;
