55ebf9b23cae4a8396e9840102144e9ce67f425a
André De Bem <andrebemferraz@gmail.com>
2021-04-28 18:26:18 -0300

    Request X - XIII

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,14 +6,14 @@ import Rating from './Rating';
 class MovieCard extends Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div>
         <img src={ imagePath } alt="" />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        <Rating />
+        <Rating rating={ rating } />
       </div>
     );
   }
