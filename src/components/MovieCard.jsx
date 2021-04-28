1aff515e5ba30b7994c0da82e42e14193f67e913
renatogsouza <renatomacedo1995@gmail.com>
2021-04-28 16:54:39 -0300

    vou dar pull do master

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
-    console.log(imagePath);
+
     return (
       <div>
         <img src={ imagePath } alt="imagePath" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    // const rating = this.props;
+    // console.log(rating);
+    return (
+      <p />
+    );
+  }
+}
+
+export default Rating;
