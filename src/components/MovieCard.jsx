15a3e8b80f8b5e6323beb55c8e77b674d679cc21
Flavio Andrade <foa12381@gmail.com>
2021-04-28 17:05:03 -0300

    adiciona classes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <ol>
+        <li>MovvieCard</li>
+      </ol>
+    );
+  }
+}
+export default MovieCard;
