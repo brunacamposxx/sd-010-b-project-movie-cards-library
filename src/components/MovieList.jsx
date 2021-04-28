559787c7622c293409c3a4e2c18a77964663e70f
isTorres <ismaeltorres612@gmail.com>
2021-04-28 18:25:00 -0300

    commit bomba :/

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import Movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        {Movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,20 @@
-// implement Rating component here
+import React from 'react';
+import Movies from '../data';
+// import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>Rating</h4>
+        <span>{ console.log(Movies.map((movie) => movie.rating)) }</span>
+      </div>
+    );
+  }
+}
+
+// Rating.propTypes = {
+//   rating: PropTypes.number.isRequired,
+// };
+
+export default Rating;
