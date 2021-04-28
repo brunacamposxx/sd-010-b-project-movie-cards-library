2c20ab54540dd034d740c462d5578e64c50b7f9a
Huggo Parcelly <h.parcelly@gmail.com>
2021-04-28 17:57:51 -0300

    adiciona o componete movieCard e as keys

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    // `title`, `subtitle`, `storyline`, `imagePath` e `rating`
+    return (
+      <section>      </section>
+    );
+  }
+}
+
+export default MovieCard;
