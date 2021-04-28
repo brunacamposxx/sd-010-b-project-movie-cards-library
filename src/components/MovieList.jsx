c094adcdf5ccaea80bff328faf765bfb7b6a3d52
programadorEmerson <programador.emerson@hotmail.com>
2021-04-28 16:24:15 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+// import "./style.css";
+// import "./function.js";
+
+const movieList = ({ titulo, subTitulo }) => (
+  <section className="nome-da-classe">
+    <h1>{titulo}</h1>
+    <p>{subTitulo}</p>
+  </section>
+);
+
+class MovieList extends Component {
+  render() {
+    return (
+      movieList({ titulo: 'Título', subTitulo: 'Sub-Titulo' })
+    );
+  }
+}
+
+export default MovieList;
