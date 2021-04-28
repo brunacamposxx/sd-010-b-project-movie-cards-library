bf54edb81631e48f0af8ca1ca97364c5f7a88b99
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-04-28 18:02:17 -0300

    Todos os requisitos

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 // Iniciando o projeto 🚀
-import React from 'react'
+import React from 'react';
 
 class Header extends React.Component {
   render() {
     return (
       <header>
-          <h1>Movie Cards Library</h1>
+        <h1>Movie Cards Library</h1>
       </header>
     );
   }
---
+++ src/components/MovieList.js
@@ -0,0 +1,26 @@
+// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return movies.map((element) => (
+      <MovieCard key={ element.title } movie={ element } />));
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      imagePath: PropTypes.string,
+      rating: PropTypes.number,
+    }),
+  ).isRequired,
+};
+
+export default MovieList;
