3e1720a6a0b6643a444f6b79043aa181327f4675
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-04-28 18:07:01 -0300

    subindo a 1 ee 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import data from '../data.js';
-import MovieList from './MovieList.jsx'
 class MovieCard extends React.Component {
-    render() {
-      return (
-          <div>
-              <p>{this.props.title}</p>
-              <p>{this.props.subtitle}</p>
-              <p>{this.props.storyline}</p>
-              <p>{this.props.rating}</p>
-              <img src={this.props.img}/>
-          </div>
-          );
-      }
+  render() {
+    return (
+      <div> vaqv </div>
+    //   <div>
+    //     <p>{ this.props.title </p>
+    //     <p>{ this.props.subtitle }</p>
+    //     <p>{ this.props.storyline }</p>
+    //     <p>{ this.props.rating }</p>
+    //     <img src={ this.props.img }/>
+    //   </div>
+    );
+  }
 }
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,50 +1,21 @@
 import React from 'react';
-import movies from '../data.js';
-import MovieCard from './MovieCard.jsx'
+// import movies from '../data.js';
+import MovieCard from './MovieCard';
 class MovieList extends React.Component {
-    render() {
+  render() {
     return (
-        <div>
-            <MovieCard />
-            {/* <MovieCard 
-            title={movies[0].title} 
-            subtitle={movies[0].subtitle}
-            storyline={movies[0].storyline}
-            rating={movies[0].rating}
-            img={movies[0].imagePath}
-            />
-            <MovieCard 
-            title={movies[1].title} 
-            subtitle={movies[1].subtitle}
-            storyline={movies[1].storyline}
-            rating={movies[1].rating}
-            img={movies[1].imagePath}
-            />
-            <MovieCard 
-            title={movies[2].title} 
-            subtitle={movies[2].subtitle}
-            storyline={movies[2].storyline}
-            rating={movies[2].rating}
-            img={movies[2].imagePath}
-            />
-            <MovieCard 
-            title={movies[3].title} 
-            subtitle={movies[3].subtitle}
-            storyline={movies[3].storyline}
-            rating={movies[3].rating}
-            img={movies[3].imagePath}
-            />
-            <MovieCard 
-            title={movies[4].title} 
-            subtitle={movies[4].subtitle}
-            storyline={movies[4].storyline}
-            rating={movies[4].rating}
-            img={movies[4].imagePath}
-            /> */}
-        </div>
-        );
-    }
-    
+      <div>
+        <MovieCard />
+        {/* <MovieCard
+          title={ movies[0].title }
+          subtitle={ ovies[0].subtitle }
+          storyline={ movies[0].storyline }
+          rating={ movies[0].rating }
+          img={ movies[0].imagePath }
+        /> */}
+      </div>
+    );
+  }
 }
 
 export default MovieList;
