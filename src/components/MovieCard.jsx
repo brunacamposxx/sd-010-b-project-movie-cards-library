4e01df2d1fb8aec31772f25b42d33accc4c15cba
Joao Vanelli <vanellijoao@gmail.com>
2021-04-28 15:15:06 -0300

    Creating all components statically

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <Rating />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React from 'react';
+
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+        <MovieCard />
+      </div>
+    );
+  }
+}
+
+export default MovieList;
