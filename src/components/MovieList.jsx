ed4f911e195b2b105cdbc6176dee79b0b4770114
viniciustec <vinialvesrocha@gmail.com>
2021-04-28 16:11:05 -0300

    1 - Crie um componente <Header /> e 2 - Renderize um texto no <Header />

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <p>{movies}</p>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
+
+export default MovieList;
