34807ed69b901ee86b58bcc9136f78b5030b284e
Mariana Savoldi <marianasavodi123@gmail.com>
2021-04-28 18:51:39 -0300

    Requisito 3 completo

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Movie List</h2>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
