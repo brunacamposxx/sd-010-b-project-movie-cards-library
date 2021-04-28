00324fac0f65e87bb5e03ce4cca453199d526021
Huggo Parcelly <h.parcelly@gmail.com>
2021-04-28 19:21:17 -0300

    adiciona a nota dos filmes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,20 @@
 // implement MovieCard component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     // `title`, `subtitle`, `storyline`, `imagePath` e `rating`
     const { movie } = this.props;
-    const { title, subtitle, storyline, imagePath } = movie;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <section>
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
         <img src={ imagePath } alt={ title } />
+        <Rating rating={ rating } />
       </section>
     );
   }
@@ -24,6 +26,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
     imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number,
   }).isRequired,
 };
 
