575970ab426ab88681e9f07d5f50b9d38a17a8c6
Leandro Reis <lleandrofr@gmail.com>
2021-04-28 17:22:30 -0300

    creates a Ratting component

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,17 @@
-// import React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// class Rating extends React.Component {
-//   render() {
-//     return (
-//       <p>{this.props.rate}</p>
-//     );
-//   }
-// }
-
-// export default Rating;
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <p className="rating">{rating}</p>
+      </div>
+    );
+  }
+}
+Rating.propTypes = {
+  key: PropTypes.number,
+}.isRequired;
+export default Rating;
