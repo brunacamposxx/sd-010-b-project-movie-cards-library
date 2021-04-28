d07b90f59d9cd876de68a862c5e241c7204bf6df
viniciustec <vinialvesrocha@gmail.com>
2021-04-28 17:48:53 -0300

    10 - Renderize a sinopse do filme dentro de uma tag p

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,6 +8,7 @@ class MovieCard extends Component {
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <img src={ imagePath } alt={ subtitle } />
+        <p>{storyline}</p>
       </div>
     );
   }
