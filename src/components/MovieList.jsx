f65abf00ed0486647ee4f815419f143ffa8ab07b
Maximiliano Alvarenga <maximilianoabispo@gmail.com>
2021-04-28 15:05:35 -0400

    Erros de lint MovieList.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import MovieCard from './MovieCard';
+import React from "react";
+import MovieCard from "./MovieCard";
 
 class MovieList extends React.Component {
-    render() {
-        const { movies } = this.props;
-        return <>
-        {movies.map((movie) => <MovieCard key={movie.title}/>)}
-        </>
-    }
+  render() {
+    const { movies } = this.props;
+    return (
+      <>
+        {movies.map((movie) => (
+          <MovieCard key={movie.title} />
+        ))}
+      </>
+    );
+  }
 }
 
 export default MovieList;
