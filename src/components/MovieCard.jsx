272d7523a8cde477ef8e27a189909d6f4b1d84a9
denis-rossati <denis.rossati@hotmail.com>
2021-04-28 15:31:59 -0300

    lint do MovieCard ajeitado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
+
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
+    const { movie: { imagePath, title, subtitle, storyline, rating } } = this.props;
     return (
       <section>
-        <img src={ movie.imagePath } alt="movie preview" />
-        <h4>{ movie.title }</h4>
-        <h5>{ movie.subtitle }</h5>
-        <p>{ movie.storyline }</p>
-        <Rating rating={ movie.rating } />
+        <img src={ imagePath } alt="movie preview" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </section>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
