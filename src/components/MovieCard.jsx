3490ca1b7593d330fe8afa87a78b0cc101e66568
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-04-28 15:21:30 -0300

    Varios Requisitos de um vez

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,31 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
+import movies from '../../../../exercicios-fixacao/Projeto-React/projeto-react/src/data';
 
 class MovieCards extends Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movies;
+    const { title, subtitle, storyline, imagePath } = this.props.movies;
 
     return (
-      <div className='moviecard'>
+      <div className="moviecard">
         <div>
-          <img src={imagePath} alt={`${title}`} />
-          <h4>{title}</h4>
-          <h5>{subtitle}</h5>
-          <p>{storyline}</p>
-          <h5>{Rating}</h5>
+          <img src = { imagePath } alt = {` ${title} `} />
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
+          <h5>{ Rating }</h5>
         </div>
       </div>
     );
-  };
-};
+  }
+}
 
-MovieCards.propTypes = {
+movies.propTypes = {
   title: PropTypes.string.isRequired,
   subtitle: PropTypes.string.isRequired,
   storyline: PropTypes.string.isRequired,
   imagePath: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
+  rating: PropTypes.number.isRequired,
+};
 
 export default MovieCards;
