62a412805f6ce6af95aa754894bb0bc6319cffb3
Jessie <miss_sc_jessi@hotmail.com>
2021-04-28 18:50:23 -0300

    requisito 11 lint

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,10 +4,9 @@ import React, { Component } from 'react';
 export class Rating extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <div />
     );
   }
-};
+}
 
 export default Rating;
