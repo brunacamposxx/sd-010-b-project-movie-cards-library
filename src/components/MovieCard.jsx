adbd0694aa57c7af717dfe2c7a4764ad07881f47
renanpbraga <renanpbraga@gmail.com>
2021-04-28 17:44:49 -0300

    adiciona alguns requisitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,33 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, rating, storyline, imagePath } = movie;
+    return (
+      <section>
+        <div>
+          <img src={imagePath} alt= {title}/>
+          <p>{title}</p>
+          <p>{subtitle}</p>
+          <p>{storyline}</p>
+        </div>
+        <div>
+          {/* <Rating rating = { rating } /> */}
+        </div>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape(),
+};
+
+MovieCard.defaultProps = {
+  movie: {},
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,21 @@
-// implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component{
+	render() {
+		return (
+			<main>
+				<ul>
+				{movies.map((movie, index) => (
+					<li>
+						<MovieCard key = { index } movie = { movie } />
+					</li>
+				))}
+				</ul>
+			</main>
+		)
+	}
+}
+
+export default MovieList;
