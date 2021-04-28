e8e3fb26fc103adc568940f95e35f19be67dbefa
Misael Bueno <misa.hbueno@gmail.com>
2021-04-28 17:20:17 -0300

    requisitos de 3 ate 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return <img src={movie.imagePath} alt=""/>
+  }
+}
+
+export default MovieCard;
