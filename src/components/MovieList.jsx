85aea054310c670ca3d500a69281b646ca369b3a
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-04-28 16:25:48 -0300

    requisitos 1, 2 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+import data from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return movies;
+  }
+}
+
+// MovieList.prototype {};
+
+export default MovieList;
