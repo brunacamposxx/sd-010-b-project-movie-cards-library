499ad28c9dfe8168f50224588db9a9a400d84430
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-04-28 20:40:18 -0300

    adicionando agradecimentos

--- src/App.js
+++ src/App.js
@@ -15,6 +15,11 @@ function App() {
 }
 
 export default App;
-
+// Agradecimento a
 // Jefferson A.
 // Mariana Mor.
+// Daniel Roberto
+// Denis Rossati
+// Murillo Wlf
+// Lucas Martins
+// e todas as outras pessoas que eu n me lembro o nome que me ajudaram a entender melhor o react e começar a gostar de utiliza-lo
