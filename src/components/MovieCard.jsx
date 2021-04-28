2a25400f4ea34e0aafe9fcf119fa3fc4a4cdf94f
André De Bem <andrebemferraz@gmail.com>
2021-04-28 18:18:24 -0300

    Request VIII - Fix II

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import Rating from './Rating';
+
 class MovieCard extends Component {
   render() {
-    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
     return (
       <div>
         <img src={ imagePath } alt="" />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
+        <Rating />
       </div>
     );
   }
 }
 
 MovieCard.propTypes = {
-  key: PropTypes.string.isRequired,
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    return (
+      <div>
+        <span>TESTE!</span>
+      </div>
+    );
+  }
+}
+
+export default Rating;
