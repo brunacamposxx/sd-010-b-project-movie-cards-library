578ef188f7a905bdcde2dc5bc86d541c5105f6f3
Elano Evaristo <elano3004@gmail.com>
2021-04-28 17:16:15 -0300

    requisitos 1 & 2 :rocket:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,19 @@
-// Iniciando o projeto 🚀
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="movie-card-header">
+      <h1 className="page-title">Movie Cards Library</h1>
+    </header>
+  );
+}
+
+export default Header;
+
+// class Header extends React.Component {
+//   render() {
+//     return <header className="movie-card-header"><h1 className="page-title">Movie Cards Library</h1></header>;
+//   }
+// }
+
+// export default Header;
