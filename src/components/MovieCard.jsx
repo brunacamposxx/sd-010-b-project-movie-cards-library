7784f16024f1c8723e0ee35c6e14fd9f3dd8769f
Leandro Xavier <leandrosxavi@gmail.com>
2021-04-28 16:43:59 -0300

    Tentando fazer o requisito 3, vao 3, varios testes passando junto. Confusao totaaaal

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React from 'react';
+// import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    // const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
+      <div className="movie-card">
+        {/* <img src={ imagePath } alt={ title } />
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p> */}
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import movies from '../data';
+// import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <main className="movie-list">
+        { movies.props }
+      </main>
+    );
+  }
+}
+
+export default MovieList;
