bdab30e9f368becee096b408fbbe4373548d0ee9
Thiago Kachinsky <tjk091@gmail.com>
2021-04-28 17:45:46 -0300

    Requisitos 14 e 15

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,14 +4,14 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
       <div>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        <Rating />
+        <Rating rating={ rating } />
       </div>
     );
   }
