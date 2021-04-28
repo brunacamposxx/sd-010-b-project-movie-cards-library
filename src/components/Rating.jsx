a514f140956c1e2579111df397032ed8fc2c5377
Leandro Xavier <leandrosxavi@gmail.com>
2021-04-28 18:08:43 -0300

    Projeto mais confuso da vida: 100da: 100 por cento concluido, mas nao ta rodando no npm start hahaha socorro

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+// import movies from '../data';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <div className="rating" />
+      <div className="rating">
+        <span className="movie-card-rating">{ rating }</span>
+      </div>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
