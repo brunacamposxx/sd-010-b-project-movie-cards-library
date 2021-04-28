ba6467980a881c37874da604c0e994c8d3d4be66
Caio <thehaseol@gmail.com>
2021-04-28 16:44:31 -0300

    Add fonts

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -13,3 +13,7 @@ Rating.propTypes = {
 };
 
 export default Rating;
+
+// Source: https://www.w3schools.com/react/react_components.asp#:~:text=Components%20are%20independent%20and%20reusable,will%20concentrate%20on%20Class%20components.
+// Ajuda na resolução por meio do Discord.
+// https://www.youtube.com/watch?v=ycrhfkZUKT0
