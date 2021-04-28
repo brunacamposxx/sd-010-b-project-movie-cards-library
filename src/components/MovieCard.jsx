b61de489e482396246795f48e0f5fea48b5bb41f
Maximiliano Alvarenga <maximilianoabispo@gmail.com>
2021-04-28 15:35:32 -0400

    Solucionado erro eslint MovieCard.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    return (
-        <h1>Olá Mundo!!!</h1>
-    );
+    return <h1>Olá Mundo!!!</h1>;
   }
 }
 
