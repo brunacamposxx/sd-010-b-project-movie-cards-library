c396eab046b01d9ba4b28059b9013aa5267ab46b
Paulo Xavier <pxavierf@gmail.com>
2021-04-28 15:16:09 -0300

    criando Componente MovieCard & Rating

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import MovieCard from './MovieCard';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
-    const { movieList } = this.props.movies;
+    const movieList = this.props.movies;
     return (
       <div>
         { movieList.map((movieInfo) => {
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,20 @@
-// implement Rating component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+
+class Rating extends React.Component {
+  render() {
+    const movieRate = this.props.rating;
+    return (
+      <div className='rating'>
+        Rating {movieRate}
+      </div>
+    );
+  }
+}
+
+// Rating.propTypes = {
+//   rating: PropTypes.number,
+// }
+
+export default Rating;
