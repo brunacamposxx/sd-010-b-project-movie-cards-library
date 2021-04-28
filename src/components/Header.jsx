37b19f76e8c738cc25946501d1e8e5dacabee54c
Caio <thehaseol@gmail.com>
2021-04-28 15:34:18 -0300

    requisito 01

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,10 @@
 // implement Header component here o/
+import React from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return <h1>movie card</h1>;
+  }
+}
+
+export default MovieCard;
