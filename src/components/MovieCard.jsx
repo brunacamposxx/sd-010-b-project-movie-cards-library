efce0fa0be87b54426abf18db4a832104d80c861
lucasportella <lucasportellaagu@gmail.com>
2021-04-28 14:54:55 -0300

    req 6,7,8,9,10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,10 +5,9 @@ class MovieCard extends React.Component {
     const {title, subtitle, storyline, rating, imagePath} = this.props.movie;
     return (
       <div>
-        <h1>{title}</h1>
-        <h2>{subtitle}</h2>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <p>{rating}</p>
         <img src={imagePath} alt={title}></img>
       </div>
     )
