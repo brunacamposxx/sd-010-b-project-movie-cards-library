79ae031c9a2e28d2e8ac9e849dda1a671a4bb068
Adão Benites <adaobjr@gmail.com>
2021-04-28 14:41:05 -0400

    Adicionado Requisitos Diversos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,41 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component { // REQUISITO 6
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
+
+    return (
+      <section>
+        <div>
+          {/* REQUISITO 7 */}
+          <img src={ imagePath } alt={ title } />
+          {/* REQUISITO 8 */}
+          <h4>{ title }</h4>
+          {/* REQUISITO 9 */}
+          <h5>{ subtitle }</h5>
+          {/* REQUISITO 10 */}
+          <p>{ storyline }</p>
+        </div>
+        <div>
+          {/* REQUISITO 13 e 14 */}
+          <Rating rating={ rating } />
+        </div>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = { // REQUISITO 17
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,19 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component { // REQUISITO 12 - Prop e Elemento <p> REQUIS 14
+  render() {
+    const { rating } = this.props;
+    return (
+      <section>
+        <p>{ rating }</p>
+      </section>
+    );
+  }
+}
+
+Rating.propTypes = { // REQUISITO 17
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
