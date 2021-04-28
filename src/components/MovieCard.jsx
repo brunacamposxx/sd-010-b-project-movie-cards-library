dbd44e6b046645bc7831423e26f06240c4420627
Matheus H. Gois <matheus.hengo@gmail.com>
2021-04-28 15:46:52 -0300

    MovieCard and MovieList body components created

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,34 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../App.css';
+
+class MovieCard extends Component {
+  render() {
+    const {
+      movie: {
+        title,
+        subtitle,
+        storyline,
+        imagePath,
+        rating,
+      },
+    } = this.props;
+    console.log(title, subtitle, storyline, imagePath, rating);
+    return (
+      <section />
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../App.css';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    console.log(movies);
+    return (
+      <section>
+        <MovieCard />
+      </section>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
