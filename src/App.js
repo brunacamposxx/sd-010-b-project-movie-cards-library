c4dcac353d0cb80944ee43c31f412b989865d5b1
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-04-28 19:45:48 -0300

    add os exercicios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+
+      <Header />
+
+      <MovieList movies={ movies } />
+
     </div>
   );
 }
+
+Header.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 export default App;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,24 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import Rating from './Rating';
+
+class MovieCard extends Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    return (
+      <section>
+        <img src={ imagePath } alt={ title } />
+        <div>
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+        </div>
+        <div>
+          <p>{ storyline }</p>
+        </div>
+        <Rating rating={ rating } />
+      </section>
+
+    );
+  }
+}
+
+export default MovieCard;
