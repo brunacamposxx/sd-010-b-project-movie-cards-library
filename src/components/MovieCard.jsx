b4c9a351a1cd04e4457cb849c8ad485a8368c769
Leandro Xavier <leandrosxavi@gmail.com>
2021-04-28 17:55:27 -0300

    Commitao bomba: Requisitos de cos de construcao dos componentes ja tinham sido realizados, porem a logica de chamadas de props e importacoes, alem de proptypes, nao havia sido implementada corretamente.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 // import movies from '../data';
 
 class MovieCard extends React.Component {
   render() {
-    // const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
       <div className="movie-card">
-        {/* <img src={ imagePath } alt={ title } />
+        <img src={ imagePath } alt={ title } />
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
-        <p className="movie-card-storyline">{ storyline }</p> */}
+        <p className="movie-card-storyline">{ storyline }</p>
+        <Rating rating={ rating } />
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
-import movies from '../data';
-// import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+// import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <main className="movie-list">
-        { movies.props }
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </main>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+// MovieList.propTypes = {
+//   movies: PropTypes.exact({
+//     title: PropTypes.string.isRequired,
+//     subtitle: PropTypes.string.isRequired,
+//     storyline: PropTypes.string.isRequired,
+//     rating: PropTypes.number.isRequired,
+//     imagePath: PropTypes.string.isRequired,
+//   }).isRequired,
+// };
+
+// MovieList.defaltProps = {
+//   movies,
+// };
 
 export default MovieList;
