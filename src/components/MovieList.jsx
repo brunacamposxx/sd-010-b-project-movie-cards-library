953c7724f811aa77992009ca90c4a3db7432cfa6
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-04-28 16:50:26 -0300

    requisitos 6 a 10

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
+    return (
+        <section>
+        {movies.map(movie => <MovieCard movie={movie}/>)}
+        </section>
+    )
   }
 };
 
