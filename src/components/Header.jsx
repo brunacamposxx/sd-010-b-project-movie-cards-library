0f2ba8061075fc66a7f9e650ac5aedcf1c5e2ac7
Daniel Ceci <danielceci96@gmail.com>
2021-04-28 19:09:40 -0300

    acredito que deu certo o requisto 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+export default Header; // eu exporto esse Header aqui e vou importar ele lá no App.js porque lá eu irei usar o Header
