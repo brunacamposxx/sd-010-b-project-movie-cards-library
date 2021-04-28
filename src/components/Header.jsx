f12b8a7f9f24e8a050bb8c0c162047e685b4e735
aythan azevedo <aythan16@gmail.com>
2021-04-28 16:19:21 -0300

    texto do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return <h1>personagens </h1>;
+    return (
+      <header>
+        <h1>personagens </h1>
+      </header>);
   }
 }
 export default Header;
