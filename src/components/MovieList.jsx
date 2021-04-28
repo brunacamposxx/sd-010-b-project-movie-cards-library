5f8e21c168db3fe8f2000896dc3cf0d0803b7145
Hugo Simoes <hugolfsimoes@gmail.com>
2021-04-28 18:50:12 -0300

    consertando tarefa 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ class MovieList extends React.Component {
     const { movies } = this.props;
     return (
       <>
-        {movies.map((movie) => <MovieCard key={ movie.title } moviegit={ movie } />)}
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </>);
   }
 }
