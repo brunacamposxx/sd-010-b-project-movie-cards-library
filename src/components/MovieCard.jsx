ed2cc28b0710d6a27bd29b9283aeb809eb0ef48e
Leandro Reis <lleandrofr@gmail.com>
2021-04-28 15:52:54 -0300

    Render < MovieCard /> components within < MovieList/> with a key

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props;
+    return (
+      <section className="movie-card">
+        <img src={ imagePath } alt="movie poster" />
+        <h3>{title}</h3>
+        <p>{subtitle}</p>
+        <p>{storyline}</p>
+        <p>{rating}</p>
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,27 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section className="movie-list">
+        { movies.map((movie) => {
+          const { title, subtitle, storyline, imagePath, rating } = movie;
+          return <MovieCard key="id" title={ title } subtitle={ subtitle } storyline={ storyline } imagePath={ imagePath } rating={ rating } />;
+        })}
+      </section>
+    );
+  }
+}
+
+export default MovieList;
+
+// (
+//   <section className="movie-list">
+//     { movies.map((movie) => {
+//       const { title, subtitle, storyline, imagePath, rating } = movie;
+//       return <MovieCard key="id" title={ title } subtitle={ subtitle } storyline={ storyline } imagePath={ imagePath } rating={ rating } />;
+//     })}
+
+//   </section>);
