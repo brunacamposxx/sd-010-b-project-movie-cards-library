1f561c54d1284330cc6c8e285d31482cb62541d4
Anderson <trkz.anderson@gmail.com>
2021-04-28 17:14:09 -0300

    PropTypes e Destruction

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,44 @@
 // implement MovieCard component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
+    const {
+      movie: {
+        title,
+        subtitle,
+        storyline,
+        imagePath,
+        rating,
+      },
+    } = this.props;
     return (
       <section>
         <header>
-          <img style={{ width: '90px' }} src={ this.props.movie.imagePath } alt="Movie" />
+          <img style={ { width: '90px' } } src={ imagePath } alt="Movie" />
         </header>
         <main>
-          <h1>{ this.props.movie.title }</h1>
-          <h5>{ this.props.movie.subtitle }</h5>
-          <p>{ this.props.movie.storyline }</p>
+          <h1>{ title }</h1>
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
         </main>
         <footer>
-          <h2>Rating</h2>
+          <h2>{ rating }</h2>
         </footer>
       </section>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,32 @@
 // implement MovieList component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
+    const { movies } = this.props;
     return (
       <div>
-        {this.props.movies.map((movieObject) => (
+        {movies.map((movieObject) => (
           <MovieCard key={ movieObject.key } movie={ movieObject } />
         ))}
       </div>
     );
   }
 }
+
+//  https://reactjs.org/docs/typechecking-with-proptypes.html
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      imagePath: PropTypes.string,
+      rating: PropTypes.number,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
