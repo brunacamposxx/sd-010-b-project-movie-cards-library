2285d16808f802ccbc6e44ff5ad267f174167f7f
Daniel <dsouzaborini@gmail.com>
2021-04-28 17:55:54 -0300

    add css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,15 +7,23 @@ class MovieCard extends React.Component {
     render() {
         const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
             return (
-                <div>
-                    <img src={ imagePath }></img>
-                    <h4>{ title }</h4>
-                    <h5>{ subtitle }</h5>
-                    <p>{ storyline }</p>
-                    <Rating rating={ rating }/>
+                <div className="movie-card">
+                    <img className="movie-card-image" src={ imagePath }></img>
+                    <h4 className="movie-card-title">{ title }</h4>
+                    <h5 className="movie-card-subtitle">{ subtitle }</h5>
+                    <p className="movie-card-storyline">{ storyline }</p>
+                    <Rating className="movie-card-rating" rating={ rating }/>
                 </div>
             )
     }
+}
+
+MovieCard.propTypes = {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
 }
 
 export default MovieCard;
