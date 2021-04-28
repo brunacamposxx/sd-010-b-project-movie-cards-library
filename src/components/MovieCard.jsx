bc134402d40b41d35f33a9655d966f3a13de530f
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-04-28 17:01:09 -0300

    Renderiza o header e o MovieList dentro do App

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -13,7 +13,7 @@ class MovieCard extends React.Component {
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <Rating rate={ rating } />
+        <Rating rating={ rating } />
 
       </div>
     );
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,20 @@
 // implement Rating component here
 import React from 'react';
-// import PropType from 'prop-types';
+import PropType from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <div />
+      <div className="rating">
+        {rating}
+      </div>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropType.number.isRequired,
+};
 
 export default Rating;
