d16e46f62fb965feac950abe8fddab3a4d628cab
Cristian <cristian.bugs@gmail.com>
2021-04-28 15:11:17 -0300

    rendering Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// implement Header component here oi
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <h1>oi</h1>
+    );
+  }
+}
+
+export default Header;
