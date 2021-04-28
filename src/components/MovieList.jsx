94360da6a02a3b6755f0bb4289a388b99d0d2d1f
Hugo Simoes <hugolfsimoes@gmail.com>
2021-04-28 18:37:38 -0300

    feito tarefas 4 ate 11

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return 0;
+    const { movies } = this.props;
+    return (
+      <>
+        {movies.map((movie) => <MovieCard key={ movie.title } moviegit={ movie } />)}
+      </>);
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
