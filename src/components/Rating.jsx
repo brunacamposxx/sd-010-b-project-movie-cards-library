15f2ed747254de76049b740aaae8bd2dd33873ef
Hugo Simoes <hugolfsimoes@gmail.com>
2021-04-28 19:02:53 -0300

    feito todas as tarefas do projeto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <h1>a</h1>
+      <p className="rating">{rating}</p>
     );
   }
 }
 
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 export default Rating;
