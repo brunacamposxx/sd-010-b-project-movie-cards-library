3eb82846a356cd88965215cc9b9ab66e5a0ac584
Anderson <trkz.anderson@gmail.com>
2021-04-28 17:34:35 -0300

    [Requisito 12] Finalizaçao do componente Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,18 @@
 // implement Rating component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
+    const { rating } = this.props;
     return (
-      <h1>Teste</h1>
+      <h6 className="rating">{ rating }</h6>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
