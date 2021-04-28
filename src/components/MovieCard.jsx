4c65fd8a0e6960c904b12c887ea6542d56d31b7d
Carlos <cemargato@gmail.com>
2021-04-28 16:52:50 -0300

    requisitos iniciais

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,23 @@
-// implement MovieCard component here
+import React from 'react';
+
+export default class MovieCard extends React.Component {
+  render() {
+    const { movieCardInfo } = this.props;
+    //   title: 'Kingsglaive',
+    //   subtitle: 'Final Fantasy XV',
+    //   storyline: 'King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire\'s plans to steal the sacred crystal.',
+    //   rating: 4.5,
+    //   imagePath: 'images/Kingsglaive_Final_Fantasy_XV.jpg',
+    
+    return (
+      <section>
+        <h4>{ movieCardInfo.title }</h4>
+        <h5>{ movieCardInfo.subtitle }</h5>
+        <p>{ movieCardInfo.storyline }</p>
+        <img src= {movieCardInfo.imagePath} alt={movieCardInfo.name}/>
+        <p>{ movieCardInfo.rating }</p>
+      </section>
+
+    );
+  }
+}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard'
+
+export default class MovieList extends React.Component{
+render() {
+  const { movieList } = this.props;
+  return(
+    <section>
+      { movieList.map ((movie)=>{
+        return  <MovieCard key= { movie.title } movieCardInfo = { movie }/>
+      })}
+   
+    </section>
+    
+  );
+}
+
+}
