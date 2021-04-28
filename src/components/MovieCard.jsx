81254a2916c9d711400bb0f6d64815aef72feed5
Huggo Parcelly <h.parcelly@gmail.com>
2021-04-28 18:53:46 -0300

    adiciona o titulo, subtitulo e sinopse

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,30 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     // `title`, `subtitle`, `storyline`, `imagePath` e `rating`
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath } = movie;
     return (
-      <section>      </section>
+      <section>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <img src={ imagePath } alt={ title } />
+      </section>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
