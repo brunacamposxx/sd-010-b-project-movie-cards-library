603f7fb049562743a594ac60db1d23304d1765a3
Matheus H. Gois <matheus.hengo@gmail.com>
2021-04-28 16:55:43 -0300

    Style "fixed"

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -17,8 +17,8 @@ class MovieCard extends Component {
     } = this.props;
     return (
       <section className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
         <section className="movie-card-body">
-          <img className="movie-card-image" src={ imagePath } alt={ title } />
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
