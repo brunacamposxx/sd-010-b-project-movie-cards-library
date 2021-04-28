11d91bdfbc9a9c39efda55a937029ffce4f724a5
Johnny-batata <you@example.com>
2021-04-28 17:59:12 -0300

    commit bomba ;-;

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,24 @@
 // implement Header component here
+import React, { Component } from 'react';
+import PropType from 'prop-types';
+
+class Header extends Component {
+  render() {
+    const { texto } = this.props;
+    return (
+      <div>
+        <header>
+          <h1>
+            {texto}
+          </h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+Header.propTypes = {
+  texto: PropType.string.isRequired,
+};
+
+export default Header;
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,32 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movies } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movies;
+    return (
+      <div>
+        <img src={ imagePath } alt="movie" />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
+
+export default MovieCard;
