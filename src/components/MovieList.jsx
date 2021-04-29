8b9523effe0bca19ae68879a8c5f741814f3557d
Vinicius <vini_bodra@hotmail.com>
2021-04-29 13:29:07 -0300

    [Vinicius Bodra] Project Movie Cards Library

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+        { movies.map((filme) => <MovieCard key={ filme.title } movies={ filme } />)}
+      </section>
+    );
+  }
+}
+
+// MovieList.propType = {
+
+// }
+
+export default MovieList;
