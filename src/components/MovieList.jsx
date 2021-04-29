890cb1c52a529eafcdbe4d77976a212bee90998b
Matheus Bodra <matheusbodra@gmail.com>
2021-04-29 19:33:50 -0300

    fazendo da forma que a trybe espera

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,16 +1,21 @@
 // implement MovieList component here
 import React from 'react';
-import Movies from '../data';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <section className="movie-list">
-        { Movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </section>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
