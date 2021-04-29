6481e0bf3bc3ebf07a4f4c87d0bde7412b9c3238
Victor Lopes <victor.cabrera0209@gmail.com>
2021-04-28 22:46:14 -0300

    First try to resolve requirement number 4 and 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,32 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    const { Card: { title, subtitle, imagePath, storyline } } = this.props;
+    return (
+      <div>
+        <h1>
+          {title}
+        </h1>
+        <h2>
+          {subtitle}
+        </h2>
+        <p>{storyline}</p>
+        <img src={ imagePath } alt={ title } />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  Card: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
-    const movies = this.props;
-    return <p>{movies[0]}</p>;
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((EachMovie) => (
+          <MovieCard key={ EachMovie.title } Card={ EachMovie } />))}
+      </div>
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
