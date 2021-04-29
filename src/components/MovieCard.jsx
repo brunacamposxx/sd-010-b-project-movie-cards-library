bb3faa19612e01b00e280bd88a74023c90694a45
Victor Lopes <victor.cabrera0209@gmail.com>
2021-04-28 23:50:44 -0300

    First try to resolve requirement number 11 to 14 and little adjustments on CSS

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    const { movie: { title, subtitle, imagePath, storyline } } = this.props;
+    const { movie: { title, subtitle, imagePath, storyline, rating } } = this.props;
     return (
-      <div>
-        <h4>
-          {title}
-        </h4>
-        <h5>
-          {subtitle}
-        </h5>
-        <p>{storyline}</p>
-        <img src={ imagePath } alt={ title } />
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={ imagePath } alt={ title } />
+          <h4 className="movie-card-title">
+            {title}
+          </h4>
+          <h5 className="movie-card-subtitle">
+            {subtitle}
+          </h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={ rating } />
+        </div>
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>
+      <div className="movie-list">
         {movies.map((EachMovie) => (
           <MovieCard key={ EachMovie.title } movie={ EachMovie } />))}
       </div>
