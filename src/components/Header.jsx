014c7449ad86ae4e9e3de8a9018ac3c04a7dc6f1
Elano Evaristo <elano3004@gmail.com>
2021-04-28 21:01:50 -0300

    esqueci de ir comitando os requisitos kkk :rocket:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,3 @@ function Header() {
 }
 
 export default Header;
-
-// class Header extends React.Component {
-//   render() {
-//     return <header className="movie-card-header"><h1 className="page-title">Movie Cards Library</h1></header>;
-//   }
-// }
-
-// export default Header;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,26 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <section className="movie-card-rating">
+        <p>Rating: </p>
+        <p className="rating">
+          { rating }
+        </p>
+      </section>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: 0,
+};
+
+export default Rating;
