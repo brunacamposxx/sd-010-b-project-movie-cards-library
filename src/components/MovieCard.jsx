59f0d450ee07bb118a4262f9034f2fd29832f88b
Vinicius <vini_bodra@hotmail.com>
2021-04-29 15:59:40 -0300

    commit semifinal

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { imagePath, title, subtitle, storyline } } = this.props;
+    const { movie: { imagePath, title, subtitle, storyline, rating } } = this.props;
     return (
       <section>
         <img src={ imagePath } alt="" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        {/* <Rating /> */}
+        <Rating rating={ rating } />
       </section>
     );
   }
@@ -22,6 +23,7 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 
