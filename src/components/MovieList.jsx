91a34b158a99e834fb3b131f5da80c363af15ba3
Felippe Correa <felippecorrea2010@gmail.com>
2021-04-29 16:12:51 -0300

    Corrigindo eslint e conseguindo 100%

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,13 @@
 // implement MovieList component here
 import React from 'react';
-import movies from '../data';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <div className="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </div>
     );
   }
