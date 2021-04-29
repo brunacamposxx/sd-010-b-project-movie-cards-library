ec95eec83aff9b6ea3c3e4c3d2d79c36daaea45c
Thiago Leite <leite.tjs@gmail.com>
2021-04-29 13:53:16 -0300

    commit bomba

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,15 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="rating">
+        <h1 className="movie-card-rating">{rating}</h1>
+      </div>
+    );
+  }
+}
+
+export default Rating;
