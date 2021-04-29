e3bea3bd0999dc8c2ef70bdc8e6e9911698a5992
Marconi <marconieng@gmail.com>
2021-04-29 13:41:28 -0300

    Adiciona os requisitos do 3 ao 17

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import data from './data';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <Header />
+      <MovieList movies={ data } />
     </div>
   );
 }
