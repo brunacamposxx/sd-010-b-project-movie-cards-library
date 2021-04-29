f147755c673bb0b36d5845378d396db525f1e60c
EderPaiva <edergomesdepaiva12@gmail.com>
2021-04-29 08:15:44 -0300

    lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,9 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (<span>alocomow</span>);
+  }
+}
+export default MovieCard;
