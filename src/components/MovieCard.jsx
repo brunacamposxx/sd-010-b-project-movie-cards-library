014d268277feaf3d80ed78b5a9dfb6d3f9ed2601
Dieghonm <dieghonm@gmail.com>
2021-04-29 02:10:38 -0300

    requisito 4, tentativa 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,18 @@
 // implement MovieCard component here
 import React from 'react';
 
+
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+
     return (
-      <ul>filmes</ul>
+      <ul>filmes
+        <li>{ title }</li>
+        <li>{ subtitle }</li>
+        <li>{ storyline }</li>
+        <li>{ imagePath }</li>
+      </ul>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,16 @@
 // implement MovieList component here
 import React from 'react';
 import MovieCard from './MovieCard';
+import Rating from './Rating'
+import movies from '../data'
 
 class MovieList extends React.Component {
   render() {
     return (
-      <body>
-        <MovieCard />
-      </body>
+      <div>
+        { movies.map(movie => <MovieCard movie = {movie} />) }
+        { movies.map(movie => <Rating movie = {movie} />) }
+      </div>
     );
   }
 }
