526d30c97d19625250814ee2b4bfada337751974
Cristian <cristian.bugs@gmail.com>
2021-04-28 22:48:18 -0300

    rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, imagePath, storyline, subtitle } } = this.props;
+    const { movie: { title, imagePath, storyline, subtitle, rating } } = this.props;
     return (
       <div>
         <img src={ imagePath } alt={ title } />
@@ -14,6 +14,9 @@ class MovieCard extends React.Component {
         <p>
           { storyline }
         </p>
+        <div>
+          { rating }
+        </div>
       </div>
     );
   }
@@ -24,7 +27,7 @@ MovieCard.propTypes = {
     imagePath: PropTypes.string,
     storyline: PropTypes.string,
     subtitle: PropTypes.string,
-    rating: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,26 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { movies: { rating } } = this.props;
+    return (
+      <div>
+        { rating }
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      imagePath: PropTypes.string,
+      storyline: PropTypes.string,
+      subtitle: PropTypes.string,
+      rating: PropTypes.number,
+    }),
+  ).isRequired,
+};
+export default Rating;
