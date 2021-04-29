759ed72e59279789639030e8ddddcd4cc743cb26
Felippe Correa <felippecorrea2010@gmail.com>
2021-04-29 15:52:55 -0300

    Fazendo estilizaçao do projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     return (
-      <div>
+      <div className="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
