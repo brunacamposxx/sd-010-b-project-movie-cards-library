fd7a655f2da6bee156305ea46c73e54a22cc5ae2
Matheus Bodra <matheusbodra@gmail.com>
2021-04-29 18:41:55 -0300

    aquele commitao bomba demandado pelo projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import Movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section className="movie-list">
+        { Movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      </section>
+    );
+  }
+}
+
+export default MovieList;
