25b031736071a292780c27d0b253cc300a79b989
tonpasso <wellpasso@gmail.com>
2021-04-29 08:39:50 -0300

    requisitos 6 a 10

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,13 @@
 // implement MovieList component here
 import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props
     return (
-      <div>       
-        <MovieList />
+      <div>
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }
