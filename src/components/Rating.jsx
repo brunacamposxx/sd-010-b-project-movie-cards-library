bd420d358ae92ae864713838c8004823ca68775c
CamilaCode7 <CamilaCode7>
2021-04-28 22:00:38 -0300

    testando react

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,19 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <section>
+        <h1 className='rating'>{ rating }</h1>
+      </section>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
