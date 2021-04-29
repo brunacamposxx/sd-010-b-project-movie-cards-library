a770f857c27f09dbb7de37b782103ebdc3751295
Dieghonm <dieghonm@gmail.com>
2021-04-29 03:33:00 -0300

    requisito muitos com propTypes, tentativa 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,15 +5,15 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, subtitle, storyline, imagePath, Rating: rating } } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
 
     return (
       <div>
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        <img src={ imagePath } alt=""/>
-        <Rating rating={ rating }/>
+        <img src={ imagePath } alt="" />
+        <Rating rating={ rating } />
       </div>
     );
   }
