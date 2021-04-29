7ccc23213801a7a92e582d51630d037587349904
Lucas Ribeiro <lucasrb04@gmail.com>
2021-04-29 01:57:40 -0300

    adicionando estilizaçoes e corrigindo rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,15 +7,14 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
-      <div>
-        <div>
-          <img src={ imagePath } alt="Filme" />
-          <h4>{title}</h4>
-          <h5>{subtitle}</h5>
-          <p>{storyline}</p>
+      <div className="movie-card">
+        <div className=".movie-card-body">
+          <img className="movie-card-image" src={ imagePath } alt="Filme" />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
         </div>
-        <div>
-          <span>Rating:</span>
+        <div className="movie-card-rating">
           <Rating rating={ rating } />
         </div>
       </div>
