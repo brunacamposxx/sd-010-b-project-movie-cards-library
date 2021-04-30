d617b3672936f84027dd8a4b1c31e7ee64c382db
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-04-30 00:49:07 -0300

    Finalizando projeto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import Data from './data';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <div className="movie-card-header page-title">
+        <Header />
+      </div>
+      <MovieList movies={ Data } />
     </div>
   );
 }
