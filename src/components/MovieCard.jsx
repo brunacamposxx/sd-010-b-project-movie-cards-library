0b0541a47f5ddd8e05aabb0a5f3bb823b70b711c
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-04-30 00:21:11 -0300

    4 - Renderize componentes `<MovieCard />` dentro de `<MovieList />` and 5 - Passe uma key para cada `<MovieCard />` renderizado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,28 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <div>
+        <p>Card</p>
+        <p>{movie.title}</p>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
