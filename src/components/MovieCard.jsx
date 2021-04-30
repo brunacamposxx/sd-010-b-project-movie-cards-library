40938f30607ceb11e2ddef31e31c46f59317e835
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-04-30 10:27:37 -0300

    tests

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import Rating from './Rating';
 
+import PropTypes from 'prop-types';
+
 class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
