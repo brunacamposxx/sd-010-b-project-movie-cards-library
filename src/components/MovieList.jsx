149ececc1da81190e2fa43c3bb94e8be0eca0b91
EderPaiva <edergomesdepaiva12@gmail.com>
2021-04-30 09:25:49 -0300

    acertando protos e lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,7 @@
 // implement MovieList component here
 import React from 'react';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
@@ -13,4 +14,13 @@ class MovieList extends React.Component {
     );
   }
 }
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })).isRequired,
+};
 export default MovieList;
