83561f7ca6ad815d14b8fae3aa401d0d1308c984
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-04-30 16:28:51 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
-// import MovieList from './components/MovieList';
-// import Data from './data';
+import MovieList from './components/MovieList';
+import Data from './data';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <MovieList movies={ Data } />
       </div>
     );
   }
