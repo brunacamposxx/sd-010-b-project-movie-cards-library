5cfa4b24a2d2aad09ad7cb4e5277b48ccbedca72
Letycia Barreto <letyciasbarreto@gmail.com>
2021-05-01 13:46:09 -0300

    R9, 10, 11,12, 13

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,21 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
         const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+        return (
         <div>
             <img src={ imagePath } alt={ title }/>
             <h4>{ title }</h4>
+            <h5>{ subtitle }</h5>
+            <p>{ storyline }</p>
+            <div>
+            <Rating rating = { rating } />
         </div>
+        </div>
+        );
     }
 }
 
