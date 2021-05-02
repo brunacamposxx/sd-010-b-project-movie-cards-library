b5cacf26528be0a064b3bd3270e17d580033f40c
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-05-01 22:09:28 -0300

    11 - Crie um componente `<Rating />`. DONE!

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,12 @@
 // implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    return (
+      <div>rating ok</div>
+    );
+  }
+}
+
+export default Rating;
