93253696b032838c3e11d3732877336e2ca8670b
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-05-01 21:50:09 -0300

    FIX: lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, storyline, rating, imagePath } = movie;
+    const { title, imagePath } = movie;
 
     return (
       <div>
