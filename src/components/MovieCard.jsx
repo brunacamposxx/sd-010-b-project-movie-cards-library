afc937ed904a27e606251cfd4e2552f571f5ff32
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-05-03 17:57:03 -0300

    Correçao requisito 12 e css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,16 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <div>
-        <h4>{ movie.title }</h4>
-        <h5>{ movie.subtitle }</h5>
-        <p>{ movie.storyline }</p>
-        <img src={ movie.imagePath } alt={ movie.title } />
-        <Rating rating={ movie.rating } />
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={ movie.imagePath } alt={ movie.title } />
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={ movie.rating } />
+        </div>
       </div>
     );
   }
