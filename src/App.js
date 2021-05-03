80afd192740a268a5d872bd224e220fa0eeed2f4
Aladino Borges <eualadino@gmail.com>
2021-05-02 23:39:16 -0300

    Refatora o App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import movies from './data';
 import Top from './components/BackTop';
 import Footer from './components/Footer';
+import movies from './data';
 import './App.css';
 
 function App() {
