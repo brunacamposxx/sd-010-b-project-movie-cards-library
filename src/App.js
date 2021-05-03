5ec2916858158f2c1c5186f518a7944c9ef6595d
Aladino Borges <eualadino@gmail.com>
2021-05-02 23:42:31 -0300

    Resolve os problemas do EsLint relativo ao footer e links.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import Top from './components/BackTop';
-import Footer from './components/Footer';
+import Top from './BackTop';
+import Footer from './Footer';
 import movies from './data';
 import './App.css';
 
