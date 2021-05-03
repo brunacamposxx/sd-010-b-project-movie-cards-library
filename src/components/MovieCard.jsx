bfd9fe48a8b45e342ea67da3462ce537e657a22e
Aladino Borges <eualadino@gmail.com>
2021-05-02 21:32:21 -0300

    MovieList.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
 // implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <span>
+        Hello
+        { movies }
+      </span>);
+  }
+}
+
+export default MovieCard;
