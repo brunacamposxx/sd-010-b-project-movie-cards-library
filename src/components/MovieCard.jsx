962951be5ac21ca2b1a5f5eb5d832946b809af12
Dângelo-Silva-Miranda <dangelo.silva.miranda@gmail.com>
2021-05-04 17:43:00 -0300

    Em MovieCard.jsx: importa o componente <Rating>; altera a ordem de renderização dentro da <section> retornada e acrescenta um componente <Rating> com a propriedade "rating". Em Rating.jsx: declara a classe Rating estendendo Components contendo o método render e o retorno de uma <section> da classe "rating" e conteúdo "rating"; implementa o propTypes e defaultProps para Rating; exporta a classe Rating.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,23 @@
 // implement MovieCard component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
       <section key={ title }>
+        <img src={ imagePath } alt={ `Capa do filme ${title}: ${subtitle}` } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
-        <img src={ imagePath } alt={ `Capa do filme ${title}: ${subtitle}` } />
         <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </section>
     );
   }
 }
+
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
