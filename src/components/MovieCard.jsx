eaebff529e996d0ed1472966a8a79329b28347e4
tomsantos07 <xotmsantosx@live.com>
2021-05-05 12:35:16 -0300

    adicionando fonte de pesquisa

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
+
 import '../App.css';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
-    const { title, subtitle, storyline, rating, imagePath } = movie;
-
+    const { movie: {
+      title,
+      subtitle,
+      imagePath,
+      storyline,
+      rating,
+    } } = this.props;
     return (
-      <div className="movie-card movie-card-body">
-        <img src={ imagePath } alt={ subtitle } className="movie-card-image" />
-        <h4 className="movie-card-title">
-          { title }
-        </h4>
-        <h5 className="movie-card-subtitle">
-          { subtitle }
-        </h5>
-        <p className="movie-card-storyline">
-          { storyline }
-        </p>
+      <section className="movie-card">
+        <section className="movie-card-body">
+          <img className="movie-card-image" src={ imagePath } alt={ title } />
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </section>
         <Rating rating={ rating } />
-      </div>
+      </section>
     );
   }
 }
