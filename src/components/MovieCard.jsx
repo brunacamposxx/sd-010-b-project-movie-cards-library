efec38f73a948d944b56bd8bb0128e1367e23cb7
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-05-05 02:23:11 -0300

    rendered component movie card into movie list

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+// import movies from '../data';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { title, imagePath } = movie;
     return (
-      null
+      <section>
+        <img src={ imagePath } alt={ title } />
+      </section>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,16 @@
+import PropTypes from 'prop-types';
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <movieCard />
+      <ol>
+        { movies.map((movie, key) => <MovieCard key={ key.title } movie={ movie } />) }
+      </ol>
     );
   }
 }
-
+MovieList.propTypes = { movies: PropTypes.arrayOf(PropTypes.object).isRequired };
 export default MovieList;
