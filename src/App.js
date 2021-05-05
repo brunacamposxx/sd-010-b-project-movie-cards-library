5eb2ac4fcf5c456dee60226e76be44c69d9bfa34
tomsantos07 <xotmsantosx@live.com>
2021-05-05 01:53:21 -0300

    corrigindo erros no movieList

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Header from './components/Header';
 import MovieList from './components/MovieList';
 import movies from './data';
+
 // import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <MovieList movie={ movies } />
+      <Header />
+      <div id="body" className="movie-list">
+        <MovieList movie={ movies } />
+      </div>
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      movies.map(() => (
+      movies.map((movie) => (
         <MovieCard movie={ movie } key={ movie.title } />
       ))
     );
