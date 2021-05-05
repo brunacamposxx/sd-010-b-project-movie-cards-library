6025e92f78b038f3221ecbdc9964ba0920588b07
André <hammel.engineer@gmail.com>
2021-05-05 11:44:43 -0300

    items 1,2,3,4,5

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,13 @@
-// implement Header component here
-// será implementado
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,32 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+// Instalando proptypes com : npm i --save prop-type
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    // 1.recebendo o array movies de data, passado em App
+    const { movies } = this.props;
+
+    // 2. fazendo o map para capturar cada elemento de movies
+    // 3. passando para MovieCard element.title como key e element(que seria cada filme) como movie
+    return (
+      movies.map((element) => <MovieCard key={ element.title } movie={ element } />));
+  }
+}
+// PropType forma 1
+// MovieList.propTypes = PropTypes.array.isRequired;
+
+// PropTypes forma 2
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
+export default MovieList;
