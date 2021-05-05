490c1f9532593d4cd45892a64725b15f9ac1d1b5
Holistic <paulofreire.si@outlook.com>
2021-05-05 06:00:28 -0300

    had some issues with arow functions, not enough time to figure it out

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,35 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: {
+      imagePath, title, subtitle, storyline, rating },
+    } = this.props;
+
+    return (
+      <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <Rating rating={ rating } />
+        </div>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,30 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/* const Rating = (props) => {
+    const { rating } = this.props;
+      return (<div className="movie-card-rating">
+        <div className="rating">
+          { rating }
+        </div>
+      </div>);
+  } */
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <div className="rating">
+          { rating }
+        </div>
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
