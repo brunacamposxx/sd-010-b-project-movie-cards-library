a2c3095100312240f82a5786f3f5060889c3b290
fredmartins <fredweb22@gmail.com>
2021-05-06 17:54:34 -0300

    renderiza texto no header

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+import React from 'react';
+import '../App.css';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section className="movie-list" />
+    );
+  }
+}
+
+export default MovieList;
