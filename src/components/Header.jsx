907c94f3cf86f41bf5b6a6651fc96b18baf2e2a2
fredmartins <fredweb22@gmail.com>
2021-05-07 12:44:00 -0300

    criando os componentes2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,8 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <h1> Movie Cards Library </h1>
-      </header>
-      // React.createElement('header', null, React.createElement('h1', null, 'Movie Cards Library'))
+      React.createElement('header', null,
+        React.createElement('h1', null, 'Movie Cards Library'))
     );
   }
 }
