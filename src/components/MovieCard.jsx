19b925f00725a34971e6b2efe2de0807b41ca110
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2021-05-10 17:37:55 -0300

    Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
   render() {
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,21 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+clas Rating extends React.Component {
+  render() {
+    const { rating } = this.prop;
+    return (
+      <div className="movie-card-rating" data-testid="rating">
+        <span className="rating">{rating}</span>
+      </div>
+    );
+  }
+}
+
+Rating.PropTypes = { rating: PropTypes.number };
+
+Rating.defaultProps = {
+  rating: 'undefined',
+};
+
+export default Rating;
