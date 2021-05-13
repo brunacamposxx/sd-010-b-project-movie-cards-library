cc99e126e43293acb8d64a0668b3b2528f82993f
Daniel Alves <araujodaniel321@gmail.com>
2021-05-13 13:51:52 -0300

    first try at exercises 3 and 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.component {
+  render() {
+    return (
+      <ul>
+        chungus
+      </ul>
+    );
+  }
+}
+
+export default MovieCard;
