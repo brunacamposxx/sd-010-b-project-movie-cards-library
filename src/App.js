d68ba84e77179a0efc7bad9b6b72d198d636065b
Daniel Alves <araujodaniel321@gmail.com>
2021-05-13 14:05:13 -0300

    fixing a little oversight

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieList from './components/MovieList';
 
 function App() {
   return (
-    <div className="header">
+    <div className="App">
       <Header />
       <MovieList />
     </div>
