eb69de6c8b16d39474d3bf1e66d97a01645f3d83
otacisos <otacisods@gmail.com>
2021-05-13 11:19:32 -0300

    componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
+    return(
+      <Header>
+        <h1>
+          Componente!
+        </h1>
+      </Header>
+    );
   }
 }
 
 export default Header;
-
-
 // implement Header component here
