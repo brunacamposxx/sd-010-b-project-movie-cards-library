b003cf90c0605b78ecff4e2bd51ab50100ae4109
Italo Matos <italomatos764@gmail.com>
2021-05-14 01:47:46 -0400

    Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,21 @@
 // implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <section>
+        <span className="rating">{rating}</span>
+      </section>
+    );
+  }
+}
+Rating.propTypes = { rating: PropTypes.number };
+
+Rating.defaultProps = {
+  rating: 'undefined',
+};
+
+export default Rating;
