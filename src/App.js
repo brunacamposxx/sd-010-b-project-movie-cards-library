093d878c9761b64a98ae9b188d9bd15c010b4ae2
otacisos <otacisods@gmail.com>
2021-05-16 16:31:58 -0300

    MovieList fix 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
+import MovieList from '.components/MovieList';
 import './App.css';
-import MovieList from '.components/Header';
 import data from './data';
 
 function App() {
