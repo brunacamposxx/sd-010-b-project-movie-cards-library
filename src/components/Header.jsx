f502b7c9fe28fecafd44b501fc7e0e09bedcf630
otacisos <otacisods@gmail.com>
2021-05-16 16:10:15 -0300

    Render Header fix-2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-        <h1>Componente!</h1>
       <Header>
         <h1> Componente!</h1>
       </Header>
