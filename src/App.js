9e3339329f3f1321bd89d869e6a4316585facc0d
otacisos <otacisods@gmail.com>
2021-05-16 18:47:34 -0300

    fix singlequote

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import Header from "./components/Header";
-import MovieList from "./components/MovieList";
-import "./App.css";
+import React from 'react';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div id="body" className="movie-list">
-        <MovieList movies={movies} />
+        <MovieList movies={ movies } />
       </div>
     </div>
   );
