a395492d00bbe7bc16ee1dc2d9f72ec390038aa9
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2021-05-16 18:28:06 -0300

    Finalizações #3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,26 +7,28 @@ class MovieCard extends React.Component {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
 
     return (
-      <div className="movie-card">
+      <section className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt={ title } />
-        <div className="movie-card-body">
+        <section className="movie-card-body">
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
-          <p className="movie-card-storyline">{storyline}</p>
-        </div>
-        <Rating rating={ rating } />
-      </div>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </section>
+        <section className="movie-card-rating">
+          <Rating rating={ rating } />
+        </section>
+      </section>
     );
   }
 }
 
 MovieCard.propTypes = {
-  movie: PropTypes.exact({
+  movie: PropTypes.shape({
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
-    rating: PropTypes.number,
     imagePath: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,23 +6,15 @@ class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>
+      <section className="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </div>
+      </section>
     );
   }
 }
 
 MovieList.propTypes = {
-  movies: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string,
-      subtitle: PropTypes.string,
-      storyline: PropTypes.string,
-      rating: PropTypes.number,
-      imagePath: PropTypes.string,
-    }),
-  ).isRequired,
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default MovieList;
