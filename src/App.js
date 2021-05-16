141a340f9332578208518d8a6f3728996224acff
otacisos <otacisods@gmail.com>
2021-05-16 16:26:28 -0300

    Movie

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
 import './App.css';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList data={ data } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return
+    <p>Movie</p>;
+  }
+}
+
+export default MovieList;
 // implement MovieList component here
