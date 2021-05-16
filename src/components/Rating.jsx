2d8d2ad2606c7caa51ea948b6b5f4149d07af636
otacisos <otacisods@gmail.com>
2021-05-16 18:43:23 -0300

    fix 9

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,7 +6,9 @@ class Rating extends React.Component {
     const { rating } = this.props;
     return (
       <div className="movie-card-rating">
-        <p className="rating">{rating}</p>
+        <p className="rating">
+          { rating }
+        </p>
       </div>
     );
   }
