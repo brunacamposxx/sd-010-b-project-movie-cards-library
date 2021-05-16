0689dac9fce8d9aa2c805843ec4e4655dead5c7e
otacisos <otacisods@gmail.com>
2021-05-16 19:16:45 -0300

    fix app-2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div id="body" className="movie-list">
-        <MovieList movies={ movies } />
+class App extends React.Components {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div id="body" className="movie-list">
+          <MovieList movies={ movies } />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
