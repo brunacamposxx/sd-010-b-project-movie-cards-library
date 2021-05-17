2992324e6fbdac71ba4c7cc0e7daad7d2ebcc31f
LucasPeixoto <peixotolucas516@gmail.com>
2021-05-16 21:32:20 -0300

    propsTypes fixed com ajuda de Henrique Zozimo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -20,7 +20,7 @@ class MovieCard extends React.Component {
   }
 }
 
-MovieCard.PropTypes = {
+MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
     subtitle: PropTypes.string,
