5454fe0373ea09e9eb4f384a1139a8a69090345c
ricardo-rals <ricardo.rals.dev@gmail.com>
2021-05-17 16:32:40 -0300

    corringindo lint e inserindo estilizaçao

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,7 +9,7 @@ class MovieCard extends React.Component {
       <section className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt={ title } />
         <div className="movie-card-body">
-          <h4 classname="movie-card-title">{ title }</h4>
+          <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
         </div>
