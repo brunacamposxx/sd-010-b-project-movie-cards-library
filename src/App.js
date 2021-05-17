df4822aff367ca3700280270d7843489de6a748e
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2021-05-17 15:38:47 -0300

    Finalizações #7

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Header/>
       <MovieList movies={ data } />
     </div>
   );
 }
 
-App();
+export default App;
