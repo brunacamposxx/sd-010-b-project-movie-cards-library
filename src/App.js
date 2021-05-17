06a101fb6a182bcbb700ac9de373f33a30c7aa46
LucasPeixoto <peixotolucas516@gmail.com>
2021-05-16 21:27:16 -0300

    imports app implementados

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import MovieCard from './components/MovieCard';
+import movies from './data';
 import MovieList from './components/MovieList';
-import Rating from './components/Rating';
 
 function App() {
   return (
-    <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
-    </div>
+    <main className="App">
+      <Header />
+      <MovieList movies={ movies } />
+    </main>
   );
 }
 
